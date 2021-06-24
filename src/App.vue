42468c25fcbd3b76aa8922cb383b8b83ea01f20d
Nicole J. Nobles <shecodez@gmail.com>
2021-06-24 08:28:10 -0400

    chore: clean up demo files

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1>HomePage</h1>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+  name: "App",
+  components: {},
+});
 </script>
 
 <style>
